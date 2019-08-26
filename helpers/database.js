97949bf82520b72c382d89ebbf6b81a9e5242ea7
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-26 06:40:03 +0530

    added userCreateIndex

--- helpers/database.js
+++ helpers/database.js
@@ -5,7 +5,7 @@ let mongoose = require("mongoose");
 exports.connection = function () {
     mongoose.connect(
         process.env.MONGODB_URI || "mongodb://localhost:27018/taskManager",
-        { useNewUrlParser: true },
+        { useNewUrlParser: true, useCreateIndex: true },
         function (err) {
             if (!err) {
                 console.log("DB Connected");
